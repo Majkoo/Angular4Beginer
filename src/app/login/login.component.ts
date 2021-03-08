5f29af5fb9f9114cf6d964795d8e38966187a79e
Szkola <pk1@ckziu.tuchow.pl>
2021-03-08 10:09:46 +0100

    commited

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-login',
+    template: '<h3> Logowanie </h3> <p> {{ description }} </p>'
+})
+
+export class LoginComponent {
+    public description = 'Podaj dane autoryzacyjne';
+}
